9039c905ac55a99c600564944b16f41f055dabe7
Esteban Mengoni <emengoni@frba.utn.edu.ar>
2023-05-11 16:23:31 -0300

    Finalizado el punto b

--- TP1/tp1.c
+++ TP1/tp1.c
@@ -1,1 +1,20 @@
-{\rtf1}
+
+
+// Punto B) Convertir una cadena de dígitos en su equivalente numérico.
+int stringToInt (char* cadena,int largoDeCadena) {
+    int numero=0;
+    int multiplicador = 1;
+    for (int i=largoDeCadena; i>=0 ;i--)
+    {
+        int aux = (cadena[i] - 48) * multiplicador;
+        multiplicador*=10;
+        numero += aux;
+        printf ("%d\n",aux);
+    }
+    return numero;
+} 
+
+int main () {
+    int numero = stringToInt ("12345",4);
+    return 0;
+}
